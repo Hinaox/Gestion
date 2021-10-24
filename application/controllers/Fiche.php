b51343630e8195210dfd9b87cee98d2527dfb48a
Hinaox <Hiaronathanael178@gmail.com>
2021-10-24 23:56:13 +0300

    yers

--- application/controllers/Fiche.php
+++ application/controllers/Fiche.php
@@ -1,26 +1,33 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Fiche extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
 	 *
 	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
+	 * 		http://example.com/index.php/Fiche
 	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
+	 * 		http://example.com/index.php/Fiche/index
 	 *	- or -
 	 * Since this controller is set as the default controller in
 	 * config/routes.php, it's displayed at http://example.com/
 	 *
 	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
+	 * map to /index.php/Fiche/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
-		
-	}		
+		$this->load->helper('url');
+		$data = array();
+		//maka anaty base
+		$data['nom'] = "RAKOTO";
+		$data['prenom'] = "ZAFY";
+		$data['matricule'] = "781227";
+		$data['photo']=  base_url("assets/photos/icon.png");
+
+		$this->load->view('fiche',$data);
+	}
 }
