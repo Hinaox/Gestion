a9cbbaddcfea0bc42f76f59212d5fa2a6d21dcfe
Kyo <80814641+kyoRaz@users.noreply.github.com>
2021-10-25 08:02:25 +0200

    Update models.php

--- application/models/models.php
+++ application/models/models.php
@@ -1,19 +1,55 @@
 <?php if(! defined('BASEPATH')) exit('NO direct script access allowed');
 class Models extends CI_Model
 {
-	public function get_info()
+	public function getListeBulletinPaie($idEmploye)
 	{
-		// On simule l'envoi d'une requete
-
-		// return array('auteur' => 'Chuck Norris',
-		// 			'date' => '24/07/05',
-		// 			'email' => 'email@ndd.fr');
-		$ligne=array();
-		$query = $this->db->query('SELECT * from fichePaie');
+        $sql = $this->db->query('SELECT * from fichePaie where idEmploye=%d');
+        $sql = sprintf($sql,$this->db->escape($idEmploye));
+        $query = $this->db->query($sql);
+		$listeBP=array();
 		foreach($query->result_array() as $row)
 		{
-			 $ligne['first_name']=$row['first_name'];
+            $bulletinDePaie=array();
+            foreach($row as $key => $value){
+                $bulletinDePaie[$key]=$value;
+            }
+            $listeBP[]=$bulletinDePaie;
 		}
-		return $ligne;
+		return $listeBP[0];
 	}
+
+    public function getBulletinDePaie($idFichePaie)
+    {
+        $sql = $this->db->query('SELECT * from fichePaie where idFichePaie=%d');
+        $sql = sprintf($sql,$this->db->escape($idFichePaie));
+        $query = $this->db->query($sql);
+		$bulletinDePaie=array();
+        
+        $query->result_array() as $row)
+		
+            $bp=array();
+            foreach($row as $key => $value){
+                $bulletinDePaie[$key]=$value;
+            }
+            $listeBP[]=$bulletinDePaie;
+		
+		return $listeBP;
+    }
+
+    public function getListeAchat($caisse){
+		$sql = "SELECT *  FROM v_achat where idCaisse=%d";
+		$sql = sprintf($sql,$this->db->escape($caisse));
+		$query = $this->db->query($sql);
+		$listeAchat = array();
+		foreach($query->result_array() as $row) {
+			$achat = array();
+			foreach($row as $key => $value){
+				$achat[$key] = $value;
+			}
+			$listeAchat[] = $achat;
+		}
+		return $listeAchat;
+	}
+
+
 }?>
