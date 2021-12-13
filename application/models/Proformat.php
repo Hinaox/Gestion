60b5821a2c34ff75a9cc51a57a66cd20e173bba9
Mendrika <mendrikaandriamitantsoa@gmail.com>
2021-12-13 07:44:26 +0100

    allproformat

--- application/models/Proformat.php
+++ application/models/Proformat.php
@@ -2,93 +2,23 @@
 
 class Proformat extends CI_Model{
 
-    // public function insert($label,$quatite,$unite,$idDepartement){
-    //     $query = "insert into demande values (null,'".$label."',".$quatite.",'".$unite."','".$idDepartement."','envoye')";
-	// 	$this->db->query($query);
-    // }
-
-    // public function getDemande()
-	// {
-    //     $sql = "SELECT id,dem.idDepartement,label,nom,quantite,unite,etat FROM demande dem 
-    //     join departement dep on dep.idDepartement = dem.idDepartement 
-    //     WHERE id not in (select idDemande from detailDemandeGrouper) order by etat";
-    //     //var_dump($sql);
-    //     $query = $this->db->query($sql);
-    //     $val = array();
-    //     $i = 0;
-    //     foreach($query -> result_array() as $row)
-    //     {
-    //         foreach($row as $key => $value)
-    //         {
-    //             $val[$i][$key] = $value;  
-    //         }
-    //         $i++;
-    //     }
-    //     return $val;
-	// }
-
-    // public function find($idDemande)
-	// {
-    //     $sql = "SELECT * from demande where id = '".$idDemande."'";
-    //     $query = $this->db->query($sql);
-    //     $val = array();
-    //     $i = 0;
-    //     foreach($query -> result_array() as $row)
-    //     {
-    //         foreach($row as $key => $value)
-    //         {
-    //             $val[$i][$key] = $value;  
-    //         }
-    //         $i++;
-    //     }
-    //     return $val;
-	// }
-
-    // public function insertDemandeGrouper($label,$qtt){
-    //     $query = "insert into demandeGrouper values (null,'".$label."',".$qtt.")";
-	// 	$this->db->query($query);
-    // }
-
-    // public function findLastEntry (){
-    //     $sql = "SELECT max(idDemandeGrouper) from demandeGrouper";
-    //     $query = $this->db->query($sql);
-    //     $val = array();
-    //     $i = 0;
-    //     foreach($query -> result_array() as $row)
-    //     {
-    //         foreach($row as $key => $value)
-    //         {
-    //             $val[$i][$key] = $value;  
-    //         }
-    //         $i++;
-    //     }
-    //     return $val;
-    // }
-
-    // public function insertDetailDemandeGrouper($label,$qtt){
-    //     $query = "insert into detailDemandeGrouper values ('".$label."',".$qtt.")";
-	// 	$this->db->query($query);
-    // }
-
-
-
-    // public function grouper($liste){
-    //     $somme =0.0;
-    //     $dem;
-    //     $label;
-    //     for($i =0 ;$i<count($liste);$i++){
-    //         $dem[]=$this->find($liste[$i]);
-    //         $somme+=$dem[$i][0]['quantite'];
-    //         $label = $dem[$i][0]['label'];
-    //     }
-    //     $this->load->model('DemandeGrouper');
-    //     $this->DemandeGrouper->insert($label,$somme,$liste);
-
-    //     // for($i=0$i<count($dem);)
-    //     //var_dump($somme);
-    // }
     public function insertProformat($dateValiditer,$label,$quantite,$prix,$idDemandeGrouper,$idFournisseur){
         $query = "insert into proformat values(null,'".$dateValiditer."','".$label."',".$quantite.",".$prix.",".$idFournisseur.",".$idDemandeGrouper.")";
         $this->db->query($query);
+    }
+    public function allProformat($idDemandeGroupe){
+        $sql = "select * from proformat where idDemandeGroupe =".$idDemandeGroupe;
+        $query = $this->db->query($sql);
+        $val = array();
+        $i = 0;
+        foreach($query -> result_array() as $row)
+        {
+            foreach($row as $key => $value)
+            {
+                $val[$i][$key] = $value;  
+            }
+            $i++;
+        }
+        return $val;
     }
 }
