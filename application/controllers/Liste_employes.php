f7c20df8e9a1ffcb35fec2f0ee130a6174b15f01
unknown <rafa.rotsy@gmail.com>
2021-12-19 16:21:23 -0100

    Update Liste_employes.php

--- application/controllers/Liste_employes.php
+++ application/controllers/Liste_employes.php
@@ -4,11 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Liste_employes extends CI_Controller {
     public  function index() {
         $this -> load -> database();
-        $this -> load -> helper('Employes');
-        $this -> load -> helper('Departement');
+        $this -> load -> model('Employe');
+        $this -> load -> model('Departement');
 
-        $liste_emp = getEmployeFromBase($this->db);
-        $liste_dept = getDepartements($this->db);
+        $liste_emp = $this->Employe->getEmployeFromBase($this->db);
+        $liste_dept = $this->Departement->getDepartements($this->db);
 
         $data = array (
             'liste_emp' => $liste_emp,
@@ -20,11 +20,11 @@ class Liste_employes extends CI_Controller {
     }
     public  function filtrer() {
         $this -> load -> database();
-        $this -> load -> helper('Employes');
+        $this -> load -> model('Employe');
 
         // echo $_POST['nom'];
 
-        $liste_emp = getEmployeFromBase($this->db, $_POST['matricule'], $_POST['nom'], $_POST['prenom'], $_POST['departement'], $_POST['poste']);
+        $liste_emp = $this->Employe->getEmployeFromBase($this->db, $_POST['matricule'], $_POST['nom'], $_POST['prenom'], $_POST['departement'], $_POST['poste']);
 
         $data = array(
             "liste_emp" => $liste_emp
@@ -35,11 +35,11 @@ class Liste_employes extends CI_Controller {
 
     public function changerDept() {
         $this -> load -> database();
-        $this -> load -> helper('Departement');
+        $this -> load -> model('Departement');
 
         $dept = $_POST['departement'];
 
-        $liste_postes = getPostes($this->db, $dept);
+        $liste_postes = $this->Departement->getPostes($this->db, $dept);
 
         $data = array (
             'liste_postes' => $liste_postes
