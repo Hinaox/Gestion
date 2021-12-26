88d71a7681fe3b9c6ab64a3ce5197e6b91fe5ecc
unknown <rafa.rotsy@gmail.com>
2021-12-26 19:00:51 -0100

    nanampy notif

--- application/views/profilEmploye.php
+++ application/views/profilEmploye.php
@@ -36,14 +36,9 @@
                       <a href = "<?php echo site_url('EtatCongeEmploye/index?idEmpl='.$emp['idEmploye']); ?>">Voir son état de congé</a>
                     </li>
                     <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                      <a href = "<?php echo site_url("ContratCtrl/index?idEmp=".$emp['idPersonne']); ?>">Etablir un contrat de travail</a>
+                      <a href = "<?php echo site_url("BPController/listeBulletinEmploye?idEmploye=".$emp['idEmploye']."&nom=".$emp['nom']) ?>">Voir son bulletin de paie</a>
                     </li>
-                    <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                      <a href = "<?php echo site_url("BPController/listeBulletinEmploye?idEmploye=".$emp['idEmploye']."&nom=".$emp['nom']) ?>">Voir son bulletin de paie</a></li>
-                    <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                      <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook mr-2 icon-inline text-primary"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>Facebook</h6>
-                      <span class="text-secondary">bootdey</span>
-                    </li>
+                    
                   </ul>
                 </div>
               <?php }else{ ?>
@@ -157,6 +152,37 @@
                     </div>
                   </div>
                 </div>
+              </div>
+
+              <div class="row gutters-sm">
+                <div class="col-sm-12 mb-3">
+                  <div class="card h-100">
+                    <div class="card-body">
+                      <h6 class="d-flex align-items-center mb-3"><i class="material-icons text-info mr-2">Notifications</i></h6>
+                      <small>Web Design</small>
+                      <div class="progress mb-3" style="height: 5px">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                      <small>Website Markup</small>
+                      <div class="progress mb-3" style="height: 5px">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: 72%" aria-valuenow="72" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                      <small>One Page</small>
+                      <div class="progress mb-3" style="height: 5px">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: 89%" aria-valuenow="89" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                      <small>Mobile Template</small>
+                      <div class="progress mb-3" style="height: 5px">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: 55%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                      <small>Backend API</small>
+                      <div class="progress mb-3" style="height: 5px">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: 66%" aria-valuenow="66" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                
               </div>
             </div>
           </div>
